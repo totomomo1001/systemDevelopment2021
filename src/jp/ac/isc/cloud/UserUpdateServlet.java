73ec36c7c5eb77fe44386f1ba761ad92236d66f7
n1200231 <cast1200231.gn.iwasaki.ac.jp>
2021-07-27 10:12:57 +0900

    練習02_2

---
+++ src/jp/ac/isc/cloud/UserUpdateServlet.java
@@ -0,0 +1,51 @@
+package jp.ac.isc.cloud;
+
+import java.io.*;
+import java.sql.*;
+
+import javax.servlet.*;
+import javax.servlet.annotation.*;
+import javax.servlet.http.*;
+
+/**
+ * Servlet implementation class UserUpdateServlet
+ */
+@WebServlet("/UserUpdateServlet")
+public class UserUpdateServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		try {
+			 Connection users = null;
+			 try {
+			 request.setCharacterEncoding("utf-8");
+			 Class.forName("com.mysql.jdbc.Driver");
+			 users = DriverManager.getConnection("jdbc:mysql://localhost/servlet_db?useUnicode=true&characterEncoding=utf8","root","");
+			 String id = request.getParameter("updateId");
+			 String name = request.getParameter("updateName");
+			 //String picture = request.getParameter("updatePicture");
+			 Statement state = users.createStatement();
+			 state.executeUpdate("UPDATE user_table SET name='" + name + "' WHERE id ='" + id + "'");
+			 state.close();
+			 users.close();
+			 response.sendRedirect("/select"); //UserSelectServletを呼び出す
+			 }catch(ClassNotFoundException e) {
+			 e.printStackTrace();
+			 }
+			 }catch(SQLException e){
+			 e.printStackTrace();
+			 }
+	}
+
+}
